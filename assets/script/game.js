1fea28fb6c8bf5c1820a068040a73d6148a3aaee
dayong <huangdayong@luckyzune.com>
2017-07-05 19:32:00 +0800

    添加了个暂停按钮以及苹果记分

--- assets/script/game.js
+++ assets/script/game.js
@@ -5,14 +5,15 @@ cc.Class({
         monkey: cc.Node,
         apple: cc.Prefab,
         monkeyImg: [cc.SpriteFrame],
-        gainApple: cc.Node
+        gainApple: cc.Node,
+        pauseButton: cc.Node,
     },
 
     // use this for initialization
     onLoad: function () {
         this.gameHeight = cc.winSize.height;
         this.dropApple();
-        this.schedule(this.dropApple, 1);
+        this.schedule(this.dropApple, 3);
         this.lastMonkeyPosition = null;
     },
 
@@ -21,7 +22,7 @@ cc.Class({
         this.node.addChild(monkey);
         monkey.x = cc.random0To1() * 610 - 180;
         monkey.y = 450;
-        this.lastMonkeyPosition = cc.p(monkey.x,monkey.y);
+        this.lastMonkeyPosition = cc.p(monkey.x, monkey.y);
         var monkeySprite = monkey.getComponent(cc.Sprite);
         monkeySprite.spriteFrame = this.monkeyImg[0];
         var down = cc.moveBy(2, cc.p(0, -180));
@@ -37,11 +38,13 @@ cc.Class({
     appleAction: function () {
         var apple = cc.instantiate(this.apple);
         this.node.addChild(apple);
-        apple.setPosition(this.lastMonkeyPosition.x + 20, this.lastMonkeyPosition.y - 80);
+        apple.setPosition(this.lastMonkeyPosition.x + 20, this.lastMonkeyPosition.y - 90);
         var down = cc.moveBy(2, cc.p(0, -180));
         var wait = cc.moveBy(0.8, cc.p(0, 0));
         var down2 = cc.moveBy(2.25, cc.p(0, -600));
-        var action = cc.sequence(down, wait, down2);
+        var rotate = cc.rotateBy(2.25, 360 * 2);
+        var downAndRotate = cc.spawn(down2, rotate);
+        var action = cc.sequence(down, wait, downAndRotate);
         apple.runAction(action);
     },
 
@@ -56,7 +59,6 @@ cc.Class({
         var redApple = this.node.getChildByName("redApple");
         if (redApple && redApple.y < - this.gameHeight / 2 - redApple.height / 2) {
             redApple.destroy();
-            // console.log("redApple destroy");
         }
         this.recordScore();
     },
@@ -64,8 +66,20 @@ cc.Class({
     recordScore: function () {
         var apples = this.gainApple.children;
         var redAppleCount = window.redAppleCount;
-        for (var i = 0; i < redAppleCount; i++) {
+        for (var i = 0; i < redAppleCount && i < apples.length; i++) {
             apples[i].opacity = 255;
+        }
+    },
+
+    pauseGame: function () {
+        var action = cc.sequence(cc.scaleTo(0.1, 3), cc.scaleTo(0.1, 1));
+        this.pauseButton.runAction(action);
+        if (cc.director.isPaused()) {
+            cc.director.resume();
+            console.log("continue");
+        } else {
+            cc.director.pause();
+            console.log("pause");
         }
     }
 });
